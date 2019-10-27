ea139eb8600cc29a9a4feacfe0dcd3c2a34952d1
Danny Chen <37634165+zhuodannychen@users.noreply.github.com>
2019-10-27 17:40:34 -0500

    Create 518_CoinChange2.java

---
+++ LeetCode/518_CoinChange2.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount+1];
+        dp[0] = 1;
+        for(int coin : coins){
+            for(int i = 0; i <= amount - coin; i++){
+                dp[i+coin] += dp[i];
+            }
+        }
+        return dp[amount];
+    }
+}
