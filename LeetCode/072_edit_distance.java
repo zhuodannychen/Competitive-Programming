263dae88a0b5c8c55037eb73595fae50c4bcf078
Danny Chen <37634165+zhuodannychen@users.noreply.github.com>
2019-12-09 18:30:52 -0600

    Create 072_edit_distance.java

---
+++ LeetCode/072_edit_distance.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        dp[0][0] = 0;
+        for (int i = 1; i < word1.length() + 1; i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 1; i < word2.length() + 1; i++) {
+            dp[0][i] = i;
+        }
+        
+        for (int i = 1; i < word1.length() + 1; i++) {
+            for (int j = 1; j < word2.length() + 1; j++) {
+                if (word1.charAt(i-1) == word2.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = Math.min(dp[i-1][j], Math.min(dp[i][j-1], dp[i-1][j-1])) + 1;
+                }
+            }
+        }
+        return dp[word1.length()][word2.length()];
+    }
+}
