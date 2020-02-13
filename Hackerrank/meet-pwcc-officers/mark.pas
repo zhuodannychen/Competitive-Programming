a99e9921265fb345395a04f2b221033cc4494d4b
Danny Chen <37634165+zhuodannychen@users.noreply.github.com>
2020-02-12 21:33:50 -0600

    Create mark.pas

---
+++ Hackerrank/meet-pwcc-officers/mark.pas
@@ -0,0 +1,15 @@
+// first time using pascal
+
+program output;
+var 
+    x:integer;
+    c:integer;
+    y:integer;
+begin
+  readln(c);
+  for x:=1 to c do
+  begin;
+  readln(y);
+  writeln(y*133);
+  end;
+end.
