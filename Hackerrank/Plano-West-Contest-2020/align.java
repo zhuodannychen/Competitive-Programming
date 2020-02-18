5c513e0a65fa27dc0b9fd7235356ea6906d82253
Danny Chen <37634165+zhuodannychen@users.noreply.github.com>
2020-02-18 11:58:43 -0600

    Create align.java

---
+++ Hackerrank/Plano-West-Contest-2020/align.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution{
+    public static final int TOGGLE = 1;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int max = 0;
+        int start = 0;
+        // 0 - >, 1 - <
+        int[] count = new int[2];
+        for (int i = 0; i < str.length(); i++) {
+            char current = str.charAt(i);
+            int index = (current == '>') ? 0 : 1;
+            max = Math.max(max, ++count[index]);
+            while (max + TOGGLE <= i - start) {
+                char s = str.charAt(start++);
+                index = (s == '>') ? 0 : 1;
+                --count[index];
+            }
+        }
+        System.out.println(str.length() - start);
+    }
+}
