6642b9225043a38409a25c6676d40e622a68c7ba
Danny Chen <37634165+zhuodannychen@users.noreply.github.com>
2020-02-18 20:34:46 -0600

    Update Solution2.java

--- Hackerrank/Plano-West-Wolf-Invitational-Contest/2020/oracle/Solution2.java
+++ Hackerrank/Plano-West-Wolf-Invitational-Contest/2020/oracle/Solution2.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+public class Solution2 {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
