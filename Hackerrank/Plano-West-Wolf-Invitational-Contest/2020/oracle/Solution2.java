f50327e47dc0d6ffade82510ed36d815bbb3ec59
Danny Chen <37634165+zhuodannychen@users.noreply.github.com>
2020-02-18 20:33:38 -0600

    Create Solution2.java

---
+++ Hackerrank/Plano-West-Wolf-Invitational-Contest/2020/oracle/Solution2.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int count = 0;
+        while (scan.hasNext())
+            if (scan.next().equals("not"))
+                ++count;
+        System.out.println(count % 2 == 0 ? "LOVE" : "SAD");
+    }
+}
