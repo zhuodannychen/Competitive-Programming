b26dde3d551a359873d4071a964953b13b606c3d
Danny Chen <37634165+zhuodannychen@users.noreply.github.com>
2020-02-18 20:43:13 -0600

    Update Solution1.java

--- Hackerrank/Plano-West-Wolf-Invitational-Contest/2020/align/Solution1.java
+++ Hackerrank/Plano-West-Wolf-Invitational-Contest/2020/align/Solution1.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class Solution{
+class Solution1 {
     public static final int TOGGLE = 1;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
